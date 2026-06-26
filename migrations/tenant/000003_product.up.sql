0c25a394c7f528747531abbedef5a9db7fcb1da6
WildanYR <wildanyahya99@gmail.com>
2026-06-26 15:53:34 +0700

    fix: correct schema for product_variant and platform_product tables

--- migrations/tenant/000003_product.up.sql
+++ migrations/tenant/000003_product.up.sql
@@ -12,10 +12,12 @@ FOR EACH ROW EXECUTE FUNCTION touch_updated_at();
 CREATE TABLE product_variant (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR NOT NULL,
-  cooldown INT NOT NULL DEFAULT 0,
-  max_user INT NOT NULL DEFAULT 5,
-  base_price INT NOT NULL DEFAULT 0,
-  product_id BIGINT NOT NULL REFERENCES product(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  duration BIGINT NOT NULL,
+  interval BIGINT NOT NULL,
+  cooldown BIGINT NOT NULL,
+  copy_template TEXT,
+  base_price BIGINT NOT NULL DEFAULT 0,
+  product_id BIGINT NOT NULL REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
   UNIQUE (product_id, name)
@@ -30,17 +32,15 @@ CREATE INDEX idx_product_variant_product_id ON product_variant (product_id);
 CREATE TABLE platform_product (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   platform VARCHAR NOT NULL,
-  sku VARCHAR NOT NULL,
-  price BIGINT NOT NULL,
+  name VARCHAR NOT NULL,
   variant VARCHAR,
-  product_id BIGINT NOT NULL REFERENCES product(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  product_variant_id BIGINT NOT NULL REFERENCES product_variant(id) ON DELETE CASCADE ON UPDATE CASCADE,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-  UNIQUE (product_id, platform)
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 ) WITH (fillfactor=90);
 
 CREATE TRIGGER platform_product_set_updated_at
 BEFORE UPDATE ON platform_product
 FOR EACH ROW EXECUTE FUNCTION touch_updated_at();
 
-CREATE INDEX idx_platform_product_product_id ON platform_product (product_id);
+CREATE INDEX idx_platform_product_product_variant_id ON platform_product (product_variant_id);
